d06066fe5db4620f3d18734cc6a24051a1aa5e02
Yashvardhan <yashvardhan.1450@gmail.com>
2026-07-05 15:56:22 +0530

    Update Home.cy.js

--- cypress/Home.cy.js
+++ cypress/Home.cy.js
@@ -3,6 +3,6 @@ describe('Home Page', () => {
     // Visit the root of your application
     cy.visit('http://localhost:5173'); // Use your frontend's dev server URL
 
-    cy.contains('h1', 'Welcome to Civix').should('be.visible');
+    cy.contains('h1', 'Welcome to CivicIQ').should('be.visible');
   });
 });
